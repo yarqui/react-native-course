03612326c4af3ff10f1799e73da75b82b1d46f48
yarqui <108986486+yarqui@users.noreply.github.com>
2023-03-25 16:39:02 +0200

    ✨add expo-splash-screen & splash img
    replace expo-app-loading with expo-splash-screen
    replace expo splash image

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Keyboard } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import fonts from "./src/utils/fonts";
 import RegistrationScreen from "./src/screens/RegistrationScreen/RegistrationScreen";
 import LoginScreen from "./src/screens/LoginScreen/LoginScreen";
+import * as SplashScreen from "expo-splash-screen";
 
 // TODO: ?? Consider making appContainer ON THE TOP of regUnderlay
 // to make top and bottom paddings global (maybe horizontal too?)
 // In this case, use regUnderlay with position: absolute?
 // Check the relative parent in this case from which it'll be positioned
 
-// TODO: implement expo-splash-screen instead of expo-app-loading
-
-const loadFonts = async () => {
-  await Font.loadAsync({
-    [fonts.robotoRegular]: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
-    [fonts.robotoMedium]: require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
-    [fonts.robotoBold]: require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
-  });
-};
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
   const [isReady, setIsReady] = useState(false);
   const [keyboardIsShown, setKeyboardIsShown] = useState(false);
   const [passwordIsShown, setPasswordIsShown] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          [fonts.robotoRegular]: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+          [fonts.robotoMedium]: require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+          [fonts.robotoBold]: require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+        });
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    loadFonts();
+  }, []);
 
   // const [windowHeight, setWindowHeight] = useState(
   //   () => Dimensions.get("window").height
@@ -56,21 +68,14 @@ const App = () => {
     Keyboard.dismiss();
   };
 
-  const submitHandler = () => {};
-
   if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
+    return null;
   }
 
   return (
     // <RegistrationScreen
     //   // windowHeight={windowHeight ? windowHeight : null}
+    //   onLayout={onLayoutRootView}
     //   keyboardIsShown={keyboardIsShown}
     //   passwordIsShown={passwordIsShown}
     //   setPasswordIsShown={setPasswordIsShown}
